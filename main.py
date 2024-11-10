62d72646108357cda56799c96699a12577297517
Loris Polenz <contact@lorispolenz.com>
2024-11-10 20:22:17 +0100

    feat(main): fix file

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ PARAMS = {
 }
 HEADERS = {
     "Accept-Encoding": "gzip, depflate",
-    "Authorization": f"Bearer {os.getenv['OPEN_DATA_API_KEY']}",
+    "Authorization": f"Bearer {os.getenv('OPEN_DATA_API_KEY')}",
 }
 
 # Initialize the S3 client
-s3_client = Minio(os.getenv['S3_ENDPOINT'],
-                  os.getenv['S3_ACCESS_KEY'],
-                  os.getenv['S3_SECRET_KEY'])
+s3_client = Minio(os.getenv('S3_ENDPOINT'),
+                  os.getenv('S3_ACCESS_KEY'),
+                  os.getenv('S3_SECRET_KEY'),
+                  secure=True)
 
 # Get the data
 response = requests.get(REQUEST_URL, params=PARAMS, headers=HEADERS)
@@ -34,7 +35,7 @@ compressed_data.seek(0)
 
 # Upload the data to S3
 s3_client.put_object(
-    bucket_name=os.getenv['S3_BUCKET'],
+    bucket_name=os.getenv('S3_BUCKET'),
     object_name=f"{TIMESTAMP}_gtfs-rt.json.gz",
     data=compressed_data,
     length=compressed_data.getbuffer().nbytes,
